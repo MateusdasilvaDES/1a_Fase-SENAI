a5e372bf2a5d7be606b97e75fb16900b951ab6f9
Mateus <mateus_silva44@estudante.sesisenai.org.br>
2024-07-01 22:35:09 -0300

    Add files via upload
    Um programa onde vc digita três notas e o programa faz uma media das notas

---
+++ media_notas.js
@@ -0,0 +1,15 @@
+/*Criar um projeto com HTML e JS, onde o usuário possa digitar três notas (inputs) e clicar em um botão para calcular a média (mostrar por alert).*/
+let nota_inpt_um = document.getElementById('InptUm')
+let nota_inpt_dois= document.getElementById('InptDois')
+let nota_inpt_tres = document.getElementById('InptTres')
+let soma_numeros, media_das_notas
+let quantidade_notas = 3
+
+function MediaNotas(){
+
+ soma_numeros = Number(nota_inpt_um.value) + Number(nota_inpt_dois.value) + Number(nota_inpt_tres.value)
+ media_das_notas = soma_numeros / quantidade_notas
+
+ alert('Essa é a media das notas: ' + media_das_notas)
+
+}
