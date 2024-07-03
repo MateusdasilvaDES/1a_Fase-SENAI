1b3c44c19f96f7c321ae1c336cc8b316231c1a58
Mateus <mateus_silva44@estudante.sesisenai.org.br>
2024-07-03 15:53:41 -0300

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,21 @@
+let nome_usuario = document.getElementById('nome')
+let email = document.getElementById('email_usuario')
+let senha_usuario = document.getElementById('senha')
+let cidade_natal_usuario = document.getElementById('cidade_natal')
+let endereco_usuario = document.getElementById('endereço')
+let nacionalidade_um = document.getElementById('brasileiro'), nacionalidade_dois = document.getElementById('estrangeiro')
+
+
+function confirmacaoCadastro(){
+
+alert(nome_usuario.value + ', seus dados foram confirmados!')
+
+}
+
+function sairCadastro(){
+
+alert(nome_usuario.value + ', você saiu tenha um bom dia!')
+
+}
+
+
